0d3dac6157362605bd374a82300b216131af5a0f
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 09:16:00 -0700

    create test for instance of vendor, test fails

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -6,5 +6,12 @@ require './lib/vendor'
 
 class VendorTest < Minitest::Test
   def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
   end
 end
