618a7837385abaf720467e52c09194eeef2ad23b
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 09:14:21 -0700

    create attributes test for item class, test passes

--- test/item_test.rb
+++ test/item_test.rb
@@ -11,4 +11,9 @@ class ItemTest < Minitest::Test
   def test_it_exists
     assert_instance_of Item, @item1
   end
+
+  def test_attributes
+    assert_equal "Peach", @item1.name
+    assert_equal "$0.75", @item1.price
+  end
 end
