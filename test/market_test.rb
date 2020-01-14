970e513594734690a11d22688a2b1924d32408cb
Elom Amouzou <elom1990@yahoo.com>
2020-01-14 09:44:18 -0700

    create add vendors test, test passes

--- test/market_test.rb
+++ test/market_test.rb
@@ -12,6 +12,8 @@ class MarketTest < Minitest::Test
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
   end
 
   def test_it_exists
@@ -21,6 +23,20 @@ class MarketTest < Minitest::Test
   def test_attributes
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
+  end
+
+  def test_add_vendors_updates_vendors
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expected = [@vendor1, @vendor2, @vendor3]
+
+    assert_equal expected, @market.vendors
   end
 
 end
